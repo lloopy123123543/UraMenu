5418e6b4b2799e51da96c4ed5b2319c26744b318
Igor Parfenov <lloopy123123543@gmail.com>
2023-09-15 21:40:51 +0500

    sus

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import menu from "./assets/styles/menu.module.scss";
+import FileMenu from "./components/FileMenu";
+import EditMenu from "./components/EditMenu";
+import InsertMenu from "./components/InsertMenu";
+import TableMenu from "./components/TableMenu";
+import FormatMenu from "./components/FormatMenu";
+import HelpMenu from "./components/HelpMenu";
+function App() {
+
+  return (
+    <div>
+      <div className={menu.top_menu}>
+        <FileMenu/>
+        <EditMenu/>
+        <InsertMenu/>
+        <FormatMenu/>
+        <TableMenu/>
+        <HelpMenu/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
